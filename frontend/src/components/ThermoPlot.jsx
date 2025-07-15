b91130bcd616d7b8fa986ba0b4afec746325445b
Auke-Pieter Colijn <auke.pieter.colijn@gmail.com>
2025-07-15 18:03:10 +0200

    working on TS plot

--- frontend/src/components/ThermoPlot.jsx
+++ frontend/src/components/ThermoPlot.jsx
@@ -2,23 +2,16 @@ import Plot from 'react-plotly.js'
 import useThermoData from '../hooks/useThermoData'
 import { getIsothermColor } from '../utils/colorScale'
 
-function ThermoPlot({ fluid, diagram }) {
-  const data = useThermoData(fluid, diagram)
-
-  if (!data) return <p style={{ color: '#666' }}>Loading...</p>
-
+function buildPHTraces(data) {
+  const traces = []
   const Tmin = Math.min(...data.isotherms.map(i => i.T))
   const Tmax = Math.max(...data.isotherms.map(i => i.T))
-
-  const traces = []
-  const xKey = diagram === 'ph' ? 'h' : 's'
-  const yKey = diagram === 'ph' ? 'p' : 'T'
 
   data.isotherms.forEach(iso => {
     const color = getIsothermColor(iso.T, Tmin, Tmax)
     traces.push({
-      x: iso[xKey],
-      y: iso[yKey],
+      x: iso.h,
+      y: iso.p,
       mode: 'lines',
       line: { color: color, dash: 'dot' },
       hovertemplate: `T = ${iso.T} K<extra></extra>`,
@@ -28,8 +21,8 @@ function ThermoPlot({ fluid, diagram }) {
 
   data.qualities.forEach(line => {
     traces.push({
-      x: line[xKey],
-      y: line[yKey],
+      x: line.h,
+      y: line.p,
       type: 'scatter',
       mode: 'lines',
       line: { color: 'red', dash: 'dash' },
@@ -38,10 +31,8 @@ function ThermoPlot({ fluid, diagram }) {
   })
 
   traces.push({
-    x: data.saturation[diagram === 'ph' ? 'hL' : 'sL'].concat(
-       data.saturation[diagram === 'ph' ? 'hV' : 'sV']
-    ),
-    y: data.saturation[yKey].concat([...data.saturation[yKey]].reverse()),
+    x: data.saturation.hL.concat(data.saturation.hV),
+    y: data.saturation.p.concat([...data.saturation.p].reverse()),
     fill: 'toself',
     type: 'scatter',
     mode: 'lines',
@@ -49,6 +40,68 @@ function ThermoPlot({ fluid, diagram }) {
     fillcolor: 'rgba(173,216,230,0.2)',
     name: 'Saturation dome'
   })
+
+  return traces
+}
+
+function buildTSTraces(data) {
+  const traces = []
+  const Tmin = Math.min(...data.isotherms.map(i => i.T))
+  const Tmax = Math.max(...data.isotherms.map(i => i.T))
+
+  data.isotherms.forEach(iso => {
+    const color = getIsothermColor(iso.T, Tmin, Tmax)
+    traces.push({
+      x: iso.s,
+      y: iso.T,
+      mode: 'lines',
+      line: { color: color, dash: 'dot' },
+      hovertemplate: `T = ${iso.T} K<extra></extra>`,
+      showlegend: false
+    })
+  })
+
+  data.qualities.forEach(line => {
+    traces.push({
+      x: line.s,
+      y: line.T,
+      type: 'scatter',
+      mode: 'lines',
+      line: { color: 'red', dash: 'dash' },
+      name: `Q=${line.Q}`
+    })
+  })
+
+  traces.push({
+    x: data.saturation.sL.concat(data.saturation.sV),
+    y: data.saturation.T.concat([...data.saturation.T].reverse()),
+    fill: 'toself',
+    type: 'scatter',
+    mode: 'lines',
+    line: { color: 'black' },
+    fillcolor: 'rgba(173,216,230,0.2)',
+    name: 'Saturation dome'
+  })
+
+  return traces
+}
+
+function ThermoPlot({ fluid, diagram }) {
+  const data = useThermoData(fluid, diagram)
+
+  if (!data) return <p style={{ color: '#666' }}>Loading...</p>
+
+  let traces = []
+  switch (diagram) {
+    case 'ph':
+      traces = buildPHTraces(data)
+      break
+    case 'ts':
+      traces = buildTSTraces(data)
+      break
+    default:
+      traces = []
+  }
 
   const layout = {
     xaxis: {
